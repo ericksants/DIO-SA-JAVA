994e18de00533b88a2e8f193cd769bb97de13a2a
Ericksants <erickwgr21@gmail.com>
2023-09-12 20:48:17 -0300

    first commit

---
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -0,0 +1,44 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) throws Exception {
+        selecaoCadidatos();
+    }
+
+    static void selecaoCadidatos() {
+
+        String[] candidatos = { "ERICK", "SANTOS", "MARIA", "JOSE", "PEDRO", "LUCAS", "MATEUS", "ANA", "JESSICA",
+                "LARISSA" };
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+
+        if (salarioBase > salarioPretendido) {
+            System.out.println("Ligar para o candidato");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("Ligar para o candidato com contra proposta");
+        } else {
+            System.out.println("Aguardando os demais resultados");
+        }
+    }
+}
