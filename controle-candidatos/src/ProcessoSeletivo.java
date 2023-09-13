6bf667873ad9b7802bacd9c479bfb67770defcd0
Ericksants <erickwgr21@gmail.com>
2023-09-12 21:34:55 -0300

    iteracoes

--- controle-candidatos/src/ProcessoSeletivo.java
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -1,8 +1,56 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        selecaoCadidatos();
+        // selecaoCadidatos();
+        // imprimirSelecionados();
+        String[] candidatos = { "ERICK", "SANTOS", "MARIA", "JOSE", "PEDRO" };
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando) {
+                tentativasRealizadas++;
+            } else {
+                System.out.println("Contato realizado");
+            }
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu)
+            System.out.println("Sucesso de contato com " + candidato + " na " + tentativasRealizadas + " Tentativa");
+        else
+            System.out.println("Falha de contato com " + candidato + " numero maximo de tentativas "
+                    + tentativasRealizadas + " realizadas ");
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
+    }
+
+    static void imprimirSelecionados() {
+        String[] candidatos = { "ERICK", "SANTOS", "MARIA", "JOSE", "PEDRO" };
+
+        for (int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("Candidato nº: " + indice + " é " + candidatos[indice]);
+        }
+
+        /*
+         * forma sem indice
+         * for (String candidato : candidatos) {
+         * System.out.println("Candidato " + candidato);
+         * }
+         */
+
     }
 
     static void selecaoCadidatos() {
